07d503948899dab7075c2ec8f1162f44eab50dbb
JA_X <envijs93@gmail.com>
2024-01-09 07:56:54 +0900

    Router 루트 Landing으로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Landing from './pages/Landing';
 import LoginForm from './components/LoginForm';
 import UserBasicData from './components/RegisterForm/UserBasicData';
 import UserInterestData from './components/RegisterForm/UserInterestData';
@@ -9,7 +9,7 @@ import UserProfileData from './components/RegisterForm/UserProfileData';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Landing />} />
       <Route path="/login" element={<LoginForm />} />
       <Route path="/register_basic" element={<UserBasicData />} />
       <Route path="/register_interest" element={<UserInterestData />} />
