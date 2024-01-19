adc8ff75517120f70fc9f7cd8bf7697536f72e93
JA_X <envijs93@gmail.com>
2024-01-19 16:12:50 +0900

    친구 추가 및 삭제 기능 추가(상세페이지)

--- src/pages/PostingDetails.jsx
+++ src/pages/PostingDetails.jsx
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 import { useLocation, useNavigate, Link } from 'react-router-dom';
-import { deletePostingData } from '../config/firebase';
+import { deletePostingData, updateData } from '../config/firebase';
+import { useState } from 'react';
 
 export default function PostingDetails() {
   const navigate = useNavigate();
@@ -8,14 +9,48 @@ export default function PostingDetails() {
   const { selectedCard } = location.state;
   const currentUser = JSON.parse(localStorage.getItem('currentUser'));
   const isWriter = selectedCard.writer === currentUser.id;
+  const [isFriend, setIsFriend] = useState(currentUser.userFriend?.includes(selectedCard.writer));
+  console.log(isFriend);
 
   console.log(selectedCard);
+
   const deletePostingHandler = async () => {
     const result = confirm('정말 삭제하시겠습니까?');
     if (result) {
       await deletePostingData(selectedCard.id);
       alert('삭제가 완료되었습니다.');
       navigate('/search');
+    }
+  };
+
+  const addFriendHandler = async () => {
+    const result = confirm(`${selectedCard.writerNickName}님을 친구 추가하시겠습니까?`);
+    if (result) {
+      const newFriend = selectedCard.writer;
+      const newData = {
+        ...currentUser,
+        userFriend: currentUser.userFriend ? [...currentUser.userFriend, newFriend] : [newFriend],
+      };
+      await updateData(currentUser.id, newData);
+      localStorage.setItem('currentUser', JSON.stringify(newData));
+      setIsFriend(!isFriend);
+      alert(`${selectedCard.writerNickName}님이 친구로 추가되었습니다!`);
+    }
+  };
+
+  const deleteFriendHandler = async () => {
+    const result = confirm(`${selectedCard.writerNickName}님을 정말 친구목록에서 삭제하시겠습니까?`);
+    if (result) {
+      const deleteTargetFriend = selectedCard.writer;
+      const myNewFriendList = [...currentUser.userFriend].filter(friend => friend !== deleteTargetFriend);
+      const newData = {
+        ...currentUser,
+        userFriend: myNewFriendList,
+      };
+      await updateData(currentUser.id, newData);
+      localStorage.setItem('currentUser', JSON.stringify(newData));
+      setIsFriend(!isFriend);
+      alert(`${selectedCard.writerNickName}님을 친구 목록에서 삭제하였습니다!`);
     }
   };
 
@@ -40,8 +75,10 @@ export default function PostingDetails() {
               </Link>
               <button onClick={deletePostingHandler}>삭제하기</button>
             </>
+          ) : isFriend ? (
+            <button onClick={deleteFriendHandler}>친구삭제</button>
           ) : (
-            <button>친구추가</button>
+            <button onClick={addFriendHandler}>친구추가</button>
           )}
         </div>
       </div>
