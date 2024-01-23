d1444c6f6a770ee3e36d61695a662b844e51c43b
JA_X <envijs93@gmail.com>
2024-01-23 17:38:46 +0900

    Home 페이지 css 작업 완성

---
+++ src/components/RegisterForm/Layout/carousel.jsx
@@ -0,0 +1,50 @@
+export default function Carousel() {
+  return (
+    <div className="carousel w-full h-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img src="/runners.jpg" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img src="/exercise.png" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img src="mountain_trail.jpg" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img src="yoga.jpeg" className="w-full" />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,9 @@
-import { styled } from 'styled-components';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { fetchLoginUserData } from '../config/firebase';
 import { logOutUser } from '../config/firebase';
-
-const HomeContainer = styled.div`
-  width: 40%;
-  border: 1px solid black;
-  padding: 20px;
-  margin: 20% auto;
-  text-align: center;
-`;
+import Footer from '../components/RegisterForm/Layout/footer';
+import Carousel from '../components/RegisterForm/Layout/carousel';
 
 export default function Home() {
   const navigate = useNavigate();
@@ -35,54 +28,92 @@ export default function Home() {
   }, []);
   console.log(currentUserDataInfo);
   const handleLogoutClick = async () => {
-    await logOutUser();
-    localStorage.removeItem('currentUser');
-    navigate('/login');
+    const result = confirm('정말 로그아웃 하시겠습니까?');
+    if (result) {
+      await logOutUser();
+      localStorage.removeItem('currentUser');
+      navigate('/login');
+    }
   };
 
   return (
-    <HomeContainer>
-      <>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <>
-            <p>환영합니다!</p>
-            <p>{currentUserDataInfo.userNickName}님!</p>
-            <button
-              onClick={() => {
-                navigate('/recommend');
-              }}>
-              추천회원
-            </button>
-            <button
-              onClick={() => {
-                navigate('/group');
-              }}>
-              내 운동 모임
-            </button>
-            <button
-              onClick={() => {
-                navigate('/search');
-              }}>
-              운동모임찾기
-            </button>
-            <button
-              onClick={() => {
-                navigate('/friend');
-              }}>
-              내운동친구
-            </button>
-            <button
-              onClick={() => {
-                navigate('/info');
-              }}>
-              내정보
-            </button>
-            <button onClick={handleLogoutClick}>로그아웃</button>
-          </>
-        )}
-      </>
-    </HomeContainer>
+    <div className="bg-sky-100 h-screen text-slate-500 font-bold flex  justify-center">
+      {loading ? (
+        <span className="loading loading-spinner loading-lg"></span>
+      ) : (
+        <div className="w-3/4 flex flex-col justify-between">
+          <div className="navbar flex justify-between bg-sky-50">
+            <div>
+              <a className="btn btn-ghost text-xl">Fit-In-Seoul</a>
+            </div>
+            <ul className="flex gap-10">
+              <li className="btn btn-ghost">
+                <button
+                  onClick={() => {
+                    navigate('/recommend');
+                  }}>
+                  추천회원
+                </button>
+              </li>
+              <li className="btn btn-ghost">
+                <button
+                  onClick={() => {
+                    navigate('/group');
+                  }}>
+                  내 운동 모임
+                </button>
+              </li>
+              <li className="btn btn-ghost">
+                <button
+                  onClick={() => {
+                    navigate('/search');
+                  }}>
+                  운동모임찾기
+                </button>
+              </li>
+              <li className="btn btn-ghost">
+                <button
+                  onClick={() => {
+                    navigate('/friend');
+                  }}>
+                  내 운동친구
+                </button>
+              </li>
+            </ul>
+            <div className="gap-2">
+              <div className="form-control">
+                <p className="text-sm font-semibold">{currentUserDataInfo.userNickName}</p>
+              </div>
+              <div className="dropdown dropdown-end">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                  <div className="w-10 rounded-full">
+                    <img alt="유저 사진" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                  </div>
+                </div>
+                <ul
+                  tabIndex={0}
+                  className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                  <li>
+                    <button
+                      className="justify-between"
+                      onClick={() => {
+                        navigate('/info');
+                      }}>
+                      Profile
+                      <span className="badge">New</span>
+                    </button>
+                  </li>
+                  <li>
+                    <button onClick={handleLogoutClick}>Logout</button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <Carousel />
+          <Footer />
+        </div>
+      )}
+    </div>
   );
 }
